5ffcd402ce2b11d26cd837d92a86a7935bdb739b
Suyash <suyu162001@gmail.com>
2022-05-11 11:44:45 +0530

    ✅Slider Component touch capability implemented!

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./styles.css";
-import { ReactComponent as Previous } from "../../icons/previous.svg";
-import { ReactComponent as Next } from "../../icons/next.svg";
-import { ReactComponent as Circle } from "../../icons/circle.svg";
+import {ReactComponent as Previous} from "../../icons/previous.svg";
+import {ReactComponent as Next} from "../../icons/next.svg";
+import {ReactComponent as Circle} from "../../icons/circle.svg";
 
 const Slider = ({images}) => {
-  
+
   const [background, setBackground] = useState(0);
+  const [touchX, setTouchX] = useState(0);
 
   const previousSlide = () => {
     if (background > 0) {
@@ -20,29 +21,54 @@ const Slider = ({images}) => {
     }
   };
 
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      // console.log("Timed out");
+      setBackground(Math.round(background));
+    }, 200)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [background])
+
+  const scrollhandler = (e) => {
+    let nbackground = background - 0.001 * e.deltaX;
+    console.log(e.deltaX);
+    if(nbackground > 0 && nbackground < images.length - 1)
+      setBackground(nbackground);
+  }
+
+  const touchhandler = (e) => {
+    let touchdeltaX = e.touches[0].pageX - touchX;
+    setTouchX(e.touches[0].pageX)
+    let nbackground = background - 0.005 * touchdeltaX;
+    if(nbackground >= 0 && nbackground <= images.length - 1){
+      setBackground(nbackground);    
+    }
+  }
+
   return (
     <div className="slider">
       <div className="slider_body">
-        <div className="slider_image_body">
-          {/* <img
-            key={images[background]?.background}
-            className={"slider_image"}
-            src={images[background]?.background}
-            alt="background"
-          /> */}
-          <div 
+        <div
+          onTouchStart={(e) => setTouchX(e.touches[0].pageX)}
+          onTouchMove={touchhandler}
+          onWheel={scrollhandler}
+          className="slider_image_body">
+          <div
             className="slider_image_array"
             style={{
-              "transform": `translateX(${-window.innerWidth*background}px)`,
+              "transform": `translateX(${-window.innerWidth * background}px)`,
             }}
-            >
-            {images.map((image, i) => (
+          >
+            {images.map((image) => (
               <img
-              key={image?.background}
-              className={"slider_image"}
-              src={image?.background}
-              alt="background"
-            />
+                key={image?.background}
+                className={"slider_image"}
+                src={image?.background}
+                alt="background"
+              />
             ))}
           </div>
         </div>
@@ -54,12 +80,11 @@ const Slider = ({images}) => {
             {images.map((image, i) => (
               <div
                 key={i}
-                onClick={()=>setBackground(i)}
-                className={`slider_position_circle ${
-                  image.background === images[background]?.background
+                onClick={() => setBackground(i)}
+                className={`slider_position_circle ${image.background === images[background]?.background
                     ? "active"
                     : "inactive"
-                }`}
+                  }`}
               >
                 <Circle />
               </div>
