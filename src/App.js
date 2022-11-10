c0ff3f21eb473c7e5a5914716dc3c5f41a755c13
Suyash <suyu162001@gmail.com>
2022-11-10 12:16:32 +0530

    [Under Maintenance] proting from heroku to render

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ import Nav from "./components/navigation";
 import { Link } from "react-router-dom";
 import NotFound from "./components/errors/notfound";
 import WebFont from 'webfontloader'
-import {useQuery} from "react-query";
-import axios from "./utils/axios"
 import ComingSoon from "./components/errors/comingsoon";
 
 function App() {
@@ -39,35 +37,6 @@ function App() {
     })
   }, []);
 
-  // const { isLoading, data, isError, error } = useQuery('me', () => {
-  const { isLoading, data } = useQuery('me', () => {
-    return axios.get('/me');
-  })
-
-  const Loggedinlinks = () => {
-    return (
-    <Link to="/blogs/create">
-      <li className="nav_link primary bigger">
-        Start Writing
-      </li>
-    </Link>
-  )}
-
-  const Loggedoutlinks = () => {
-    return (
-    <>
-      <Link to="/login">
-        <li className="nav_link">
-          login
-        </li>
-      </Link>
-      <Link to="/signup">
-        <li className="nav_link primary">
-          signup
-        </li>
-      </Link></>
-  )}
-
   const Maintenance = () => <ComingSoon message="We are porting fraise from heroku to render. This might take 24 hours."/>
 
   return (
@@ -79,9 +48,9 @@ function App() {
               blogs
             </li>
           </Link>
-          { !isLoading ? 
+          {/* !isLoading ? 
               data.data.user === null ? <Loggedoutlinks/> : <Loggedinlinks/>
-            : <Loggedoutlinks/> }
+              : <Loggedoutlinks/> */}
         </Nav>
         <Switch>
           <Route path="/" exact component={Maintenance} />
