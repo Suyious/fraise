183c26cd73921b9b55893487ff7ff198fa81bf53
Suyash K <suyu162001@gmail.com>
2023-11-08 19:59:02 +0530

    fix: filter tabs in /blog and floating modal in /blog/[id]

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL = process.env.NODE_ENV === 'production' ? 
   "https://fraise.onrender.com":
-  // "https://fraise.up.railway.app":
+  // "https://fraise.onrender.com"
   "http://localhost:8080"
 
 const instance = axios.create({
