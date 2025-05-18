0a25d574e01016527dc92e7c51e5c49cb89111f3
Suyash K. <suyu162001@gmail.com>
2025-05-19 01:43:46 +0530

    fix: update production base URL in axios instance

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const baseURL = process.env.NODE_ENV === 'production' ? 
-  "https://fraise.onrender.com":
-  // "https://fraise.onrender.com"
+  "https://fraise-backend.vercel.app/":
+  // "https://fraise-backend.vercel.app/"
   "http://localhost:8080"
 
 const instance = axios.create({
